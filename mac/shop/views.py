75551cf4109a52067d7cab2317a2ae1312c94877
Ansh Agarwal <anshagrawal12348@gmail.com>
2023-01-21 15:39:29 +0530

    updating files

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -33,6 +33,33 @@ def index(request):
         allProds.append([prod,range(1,nslides),nslides])
     params={'allProds':allProds}
     return render(request,'shop/index.html',params)
+def searchMatch(query,item):
+    # return True only if quary matches the item
+    if query.lower() in item.desc.lower() or query.lower() in item.product_name.lower() or query.lower() in item.category.lower():
+        return True
+    else:
+        return False
+def search(request):
+    query= request.GET.get('search')
+    print(query)
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    # print(catprods)
+    cats = {item['category'] for item in catprods}
+    # print(cats)
+    for cat in cats:
+        prodtemp = Product.objects.filter(category=cat)
+        prod=[item for item in prodtemp if searchMatch(query , item)]
+        n = len(prod)
+        nslides = math.ceil(n / 4)
+        if n!=0:
+            allProds.append([prod, range(1, nslides), nslides])
+    # print(allProds)
+
+    params = {'allProds': allProds,"msg":""}
+    if len(allProds)==0 or len(query)<4:
+        params={'msg':'Please make sure to enter releavent search quary'}
+    return render(request, 'shop/search.html', params)
 
 def about(request):
     return render(request,'shop/about.html')
@@ -89,8 +116,6 @@ def tracker(request):
     return render(request, 'shop/tracker.html')
 
 
-def search(request):
-    return render(request,'shop/search.html')
 
 def productView(request,myid):
     #fetch the product using id
